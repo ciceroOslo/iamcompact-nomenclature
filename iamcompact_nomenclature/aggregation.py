6a281e8d7f2ee16bb962ae8c192084015f69429d
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2024-02-13 20:34:51 +0100

    Changed signature/spec of aggregation.check_var_aggregates
    Also removed the previous GitHub Copilot-generated code.

--- iamcompact_nomenclature/aggregation.py
+++ iamcompact_nomenclature/aggregation.py
@@ -10,26 +10,24 @@ import pyam
 def check_var_aggregates(
         iamdf: pyam.IamDataFrame,
         variables: Optional[Sequence[str]] = None,
-        num_recurse_sublevels: Optional[int] = None,
+        num_sublevels: Optional[int] = None,
         tolerance: Optional[float] = 0.01,
         require_complete: bool = True,
-        add_other: bool = False,
-        other_label: str = 'Other',
-        return_added_only: bool = False,
-        process_inplace: bool = False,
         variable_dimname: str = 'variable',
-) -> tuple[pyam.IamDataFrame, pd.DataFrame]:
-    """Check aggregated variables in an `IamDataFrame` or add missing parts.
+) -> tuple[pd.DataFrame|None, dict[str, list[str]]]:
+    """Check aggregated variables in an `IamDataFrame`.
     
     The function will check whether aggregated variables are sums of their
     component variables, or optionally just whether the component variables sum
-    up to less than the aggregated variable. If components sum up to less than
-    the aggregated variable, the function can optionally add the difference as a
-    new variable ("Other").
+    up to less than the aggregated variable. 
 
-    It will return a tuple of a processed `IamDataFrame` (either the complete
-    dataframe with added "Other" variables, or just the added "Other" variables)
-    and a `pandas.DataFrame` with the items that failed the checks.
+    If any of the component variables add up to something other than or greter
+    than (depending on `require_complete`) the aggregated variable, the function
+    will return a tuple of a `pandas.DataFrame` with the items that failed the
+    check along with the aggregate variable value and the component sum, and a
+    dictionary of the component variables that were summed and compared to the
+    aggregate variable. If all checks pass, the first component of the returned
+    tuple will be `None`.
 
     Parameters
     ----------
@@ -38,8 +36,9 @@ def check_var_aggregates(
     variables : sequence of str, optional
         The aggregated variables to check. If not provided, the function will
         check all top-level variables (i.e., variables that don't have a parent
-        variable).
-    num_recurse_sublevels : int, optional
+        variable), and all aggregated variables down to the level specified by
+        `num_sublevels`. Defaults to `None`.
+    num_sublevels : int, optional
         How deep to recurse to aggregated subvariables. Set to 0 to check only
         the variables in `variables` against the sum of components one level
         below, and not check any of the components that are aggregates of deeper
@@ -57,38 +56,24 @@ def check_var_aggregates(
         do want to check that the aggregate is not smaller than the sum of the
         included components. Defaults to `True` (i.e., require that the
         components sum up to the aggregated variable).
-    add_other : bool, optional
-        Whether to add a new variable with the difference between the aggregated
-        variable and the sum of its components. If True, `require_complete` is
-        ignored. Defaults to `False`.
-    other_label : str, optional
-        The label to use for the "Other" variable if `add_other` is True.
-        Defaults to "Other".
-    return_added_only : bool, optional
-        Whether to return only the added "Other" variables, or the complete
-        dataframe with added "Other" variables. Defaults to `False`. Note that
-        if this is True and `add_other` is False, the first component of the
-        returned tuple will be `None`.
-    process_inplace : bool, optional
-        Whether to process the dataframe in place. Defaults to `False`. Ignored
-        if `return_added_only` is True.
     variable_dimname : str, optional
         The name of the variable dimension in the `IamDataFrame`. Defaults to
         "variable".
 
     Returns
     -------
-    processed_iamdf, failed_checks : tuple[pyam.IamDataFrame, pandas.DataFrame]
-        A tuple of the processed `IamDataFrame` (either the complete dataframe
-        with added "Other" variables, or just the added "Other" variables) and a
-        `pandas.DataFrame` with the items that failed the checks.
-        `failed_checks` has the following three columns:
+    (failed_checks, aggregation_map)
+    failed_checks : pandas.DataFrame or None
+        A dataframe with the items that failed the checks. The columns are:
         - "aggregate": The value of the aggregated variable
         - "components": The sum of the components
-        - "component_vars": A list of the component variables that were included
-            in the sum
         The index is equal to the index for each datapoint of the aggregated
-        variable that failed the check.
+        variable that failed the check. If all checks pass, this will be `None`.
+    aggregation_map : dict
+        A dictionary of the component variables that were summed and compared to
+        each aggregate variable. The keys are the aggregated variables, and the
+        values are lists of the component variables that were included in the
+        sum.
     """
     # Get the variables to check
     if variables is None:
