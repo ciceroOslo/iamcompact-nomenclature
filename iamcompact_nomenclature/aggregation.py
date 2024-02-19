73cdad8d2f8979d1330e206a66e59d548ba5c4c8
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2024-02-19 14:07:53 +0100

    Finished aggregation.check_var_aggregates, not tested.
    The function `find_missing_aggregate_vars` in the aggregation.py module is
    still empty and waiting to be implemented.

--- iamcompact_nomenclature/aggregation.py
+++ iamcompact_nomenclature/aggregation.py
@@ -1,6 +1,7 @@
 """Module for computing and checking aggregations of variables."""
 from typing import Optional
 from collections.abc import Sequence
+import itertools
 
 import pandas as pd
 import pyam
@@ -13,9 +14,10 @@ def check_var_aggregates(
         iamdf: pyam.IamDataFrame,
         variables: Optional[Sequence[str]] = None,
         num_sublevels: Optional[int] = None,
-        tolerance: Optional[float] = 0.01,
         require_complete: bool = True,
         variable_dimname: str = 'variable',
+        method: str = 'sum',
+        **kwargs,
 ) -> tuple[pd.DataFrame|None, dict[str, list[str]]]:
     """Check aggregated variables in an `IamDataFrame`.
     
@@ -45,11 +47,6 @@ def check_var_aggregates(
         the variables in `variables` against the sum of components one level
         below, and not check any of the components that are aggregates of deeper
         sublevels. Set to `None` to check all sublevels. Defaults to `None`.
-    tolerance : float, optional
-        The tolerance for the check. If the difference between the aggregated
-        variable and the sum of its components is greater than this value, the
-        check will fail. Note that this is *absolute* difference, not the
-        relative ratio. Defaults to 0.01.
     require_complete : bool, optional
         Whether to require that the components sum up to the aggregated
         variable, or just that they not sum up to anything *greater than* the
@@ -61,30 +58,86 @@ def check_var_aggregates(
     variable_dimname : str, optional
         The name of the variable dimension in the `IamDataFrame`. Defaults to
         "variable".
+    method : str, optional
+        The method to use for finding the component variables (passed to
+        `pyam.IamDataFrame.check_aggregate`). Defaults to "sum".
+    rtol : float, optional
+        Relative tolerance for the check. Passed to `numpy.isclose`, see the
+        documentation of that function for details. At the time of writing, the
+        default value is `1e-5`. Note that the relative and absolute tolerances
+        are added together to produce the actual tolerance threshold.
+    atol : float, optional
+        Absolute tolerance for the check. Passed to `numpy.isclose`, see the
+        documentation of that function for details. At the time of writing, the
+        default value is `1e-8`. Note that the relative and absolute tolerances
+        are added together to produce the actual tolerance threshold.
 
     Returns
     -------
-    (failed_checks, aggregation_map)
-    failed_checks : pandas.DataFrame or None
-        A dataframe with the items that failed the checks. The columns are:
-        - "aggregate": The value of the aggregated variable
-        - "components": The sum of the components
-        The index is equal to the index for each datapoint of the aggregated
-        variable that failed the check. If all checks pass, this will be `None`.
+    (failed_checks, aggregation_map) failed_checks : pandas.DataFrame or None
+        A dataframe with the items that failed the checks. The columns are: -
+        "variable": The value of the aggregated variable - "components": The sum
+        of the components The index is equal to the index for each datapoint of
+        the aggregated variable that failed the check. This DataFrame is the
+        same as that returned by `IamDataFrame.check_aggregate`. If all checks
+        pass, this will be `None`.
     aggregation_map : dict
         A dictionary of the component variables that were summed and compared to
         each aggregate variable. The keys are the aggregated variables, and the
         values are lists of the component variables that were included in the
         sum.
     """
+    invalid_kwarg_keys = [_kwarg_key for _kwarg_key in kwargs
+                          if _kwarg_key not in ['rtol', 'atol']]
+    if len(invalid_kwarg_keys) > 0:
+        raise TypeError(
+            'Invalid keyword argument' + ('s' if len(invalid_kwarg_keys) > 1 else '') +
+            f' for check_var_aggregates: {", ".join(invalid_kwarg_keys)}' +
+            '. The only valid keyword arguments beyond the ones in the ' +
+            'function signature are "rtol" and "atol".'
+        )
     if variables is None:
         variables = [
-            _var for _var in iamdf.variable if var_utils.get_aggregate_variable(
+            _var for _var in iamdf.variable if var_utils.get_aggregate_var(
                 varname=_var,
                 iamdf=iamdf,
                 check_all_levels=False,
             ) is None
         ]
+    if num_sublevels != 0:
+        vars_to_check: Sequence[str] = list(itertools.chain.from_iterable([
+            var_utils.get_component_vars(
+                varname=_var,
+                iamdf=iamdf,
+                num_sublevels=num_sublevels,
+                variable_dimname=variable_dimname,
+            ) for _var in variables
+        ]))
+    else:
+        vars_to_check: Sequence[str] = variables
+    failed_checks_df_list: list[pd.DataFrame] = list()
+    aggregation_map: dict[str, list[str]] = dict()
+    for _var in vars_to_check:
+        _subvars: list[str] = var_utils.get_component_vars(
+            varname=_var,
+            iamdf=iamdf,
+            num_sublevels=1,
+            variable_dimname=variable_dimname,
+        )
+        if(len(_subvars) > 0):
+            _failed_checks = iamdf.check_aggregate(
+                variable=_var,
+                method=method,
+                **kwargs,
+            )
+            if _failed_checks is not None:
+                failed_checks_df_list.append(_failed_checks)
+            aggregation_map[_var] = _subvars
+    failed_checks_df: pd.DataFrame|None = None \
+        if len(failed_checks_df_list) == 0 else pd.concat(
+            failed_checks_df_list,
+        )
+    return failed_checks_df, aggregation_map
 
 
 def find_missing_aggregate_vars(
