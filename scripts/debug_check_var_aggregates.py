c195b23b2add0b2ceac5049a3a350ed9e2a5608a
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2024-03-13 14:39:32 +0100

    Interim commit of debugging script. Not for production.

--- scripts/debug_check_var_aggregates.py
+++ scripts/debug_check_var_aggregates.py
@@ -16,8 +16,8 @@ from iamcompact_nomenclature import var_utils
 # Get data file
 data_file: Path = Path.home() / 'src' / 'repos' / 'pyam_analyses' / 'data' \
     / 'IAM_COMPACT' / 'studies' / 'S01_NECPs' \
-        / 'iam_compact_st1__GCAM_iamc_report.xlsx'
-        # / 'Report_IAM_COMPACT_PROMETHEUS_regions_nolinksv2.xlsx'
+        / 'Report_IAM_COMPACT_PROMETHEUS_regions_nolinksv2.xlsx'
+        # / 'iam_compact_st1__GCAM_iamc_report.xlsx'
 idf: pyam.IamDataFrame = pyam.IamDataFrame(data_file)
 
 # %%
@@ -26,17 +26,28 @@ dsd: nomenclature.DataStructureDefinition = icnom.get_dsd()
 regproc: nomenclature.RegionProcessor = icnom.get_region_processor()
 
 # %%
-# Do a test check
-res = icnom.aggregation.check_var_aggregates(iamdf=idf, dsd=dsd)
+# Do a test variable aggregation check
+var_res: icnom.aggregation.VarAggregationCheckResult = \
+    icnom.aggregation.check_var_aggregates(iamdf=idf, dsd=dsd)
+
+# %%
+# Do a test region aggregation check
+reg_res: icnom.aggregation.RegionAggregationCheckResult = \
+    icnom.aggregation.check_region_aggregates(
+        iamdf=idf,
+        dsd=dsd,
+        processor=regproc,
+    )
 
 # %%
 # Test aggregation of selected variables
 failed_checks_partial: pd.DataFrame|None
 aggregation_map_partial: dict[str, list[str]]
-failed_checks_partial, aggregation_map_partial = check_var_aggregates(
-    iamdf=idf,
-    variables=['Capacity|Electricity']
-)
+failed_checks_partial, aggregation_map_partial = \
+    icnom.aggregation.check_var_aggregates_manual(
+        iamdf=idf,
+        # variables=['Capacity|Electricity']
+    )
 
 # %%
 # Test aggregation of all variables
