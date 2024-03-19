c97e052daba12a0a8fd3b288c325b0e221de5e80
Jan Ivar Korsbakken <janko@cicero.oslo.no>
2024-03-19 17:08:23 +0100

    New cells in result files nomenclature exploration script

--- scripts/explore_result_files_for_nomenclature.py
+++ scripts/explore_result_files_for_nomenclature.py
@@ -60,6 +60,8 @@ relpaths: dict[Path, Path] = {
     _p: _p.relative_to(data_root)
     for _p in data_root.glob('**/*.xlsx')
 }
+_relpath: Path
+_abspath: Path
 for _abspath, _relpath in relpaths.items():
     _idf = open_multisheet_iamc(_abspath)
     data_dict[str(_relpath)] = _idf
@@ -68,15 +70,24 @@ for _abspath, _relpath in relpaths.items():
 # Flatten the dict by inserting an @ sign in front of tab names for files that
 # have multiple tabs
 flat_data_dict: dict[str, pyam.IamDataFrame | Exception] = dict()
-for _relpath, _idf in data_dict.items():
+_pathkey: str
+for _pathkey, _idf in data_dict.items():
     if isinstance(_idf, dict):
         for _tabname, _idf_tab in _idf.items():
             flat_data_dict[f'{_relpath}@{_tabname}'] = _idf_tab
     else:
-        flat_data_dict[_relpath] = _idf
+        flat_data_dict[_pathkey] = _idf
+
+# %%
+# Get only the items that are IamDataFrames
+flat_data_dict_iamdfs: dict[str, pyam.IamDataFrame] = {
+    _relpath: _idf for _relpath, _idf in flat_data_dict.items()
+    if isinstance(_idf, pyam.IamDataFrame)
+}
 
 # %%
 # Find all the scenarios in the data files
+_datafile: str
 file_scenarios: dict[str, list[str]] = {
     _datafile: _idf.scenario for _datafile, _idf in flat_data_dict.items()
     if isinstance(_idf, pyam.IamDataFrame)
@@ -93,3 +104,46 @@ file_variables: dict[str, list[str]] = {
 # Get a dsd and region processor
 dsd: nomenclature.DataStructureDefinition = icnom.get_dsd()
 regproc: nomenclature.RegionProcessor = icnom.get_region_processor()
+
+# %%
+# Get invalid variable names from all the files, using iamcompact_nomenclature.validation
+check_dim: str = 'variable'
+invalid_names: dict[str, list[str]] = {
+    _pathkey: icnom.validation.get_invalid_names(_idf)[check_dim]
+    for _pathkey, _idf in flat_data_dict_iamdfs.items()
+}
+
+# %%
+# Go the other way: For each invalid variable name, find the files that contain
+# it. Create a dict with invalid variable names as keys, and a list of file
+# IDs as values.
+invalid_var_files: dict[str, list[str]] = {
+    _varname: [
+        _pathkey for _pathkey, _invalids in invalid_names.items()
+        if _varname in _invalids
+    ]
+    for _varname in sorted(list(set().union(*invalid_names.values())))
+}
+
+# %%
+# Create a dict with file ids as keys and *all* variable names for each file as
+# values
+file_allvars: dict[str, list[str]] = {
+    _pathkey: _idf.variable
+    for _pathkey, _idf in flat_data_dict_iamdfs.items()
+}
+
+# %%
+# Then create the opposite dict: With variable names as keys, and a list of file
+# ids as values
+# PT = tp.TypeVar('PT')
+# def force_type(x, t: tp.Type[PT]) -> PT:
+#    return x
+
+var_files = {
+    _varname: [
+        _pathkey for _pathkey, _allvars in file_allvars.items()
+        if _varname in _allvars
+    ]
+    for _varname in sorted(list(set().union(*file_allvars.values())))
+}
